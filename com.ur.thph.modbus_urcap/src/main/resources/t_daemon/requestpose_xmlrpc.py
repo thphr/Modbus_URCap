cda33375c0bff1154f198f97bc97e46f0aaa9165
TP <thphr16@student.sdu.dk>
2021-03-04 16:43:54 +0100

    python file update

--- com.ur.thph.modbus_urcap/src/main/resources/t_daemon/requestpose_xmlrpc.py
+++ com.ur.thph.modbus_urcap/src/main/resources/t_daemon/requestpose_xmlrpc.py
@@ -4,6 +4,9 @@ import sys
 import socket
 import struct
 import time
+import minimalmodbus as modbus
+import serial 
+import logging as Logger
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from SocketServer import ThreadingMixIn
@@ -11,20 +14,38 @@ from SocketServer import ThreadingMixIn
 isShowing = False
 LOCALHOST = "127.0.0.1"
 
-def showpopup():
-  global isShowing
-  isShowing = True
-  return isShowing
+instrument = None 
+value = ""
 
-def isEnabled():
-  global isShowing
-  return isShowing
+def set_modbus_tool_communication(enabled,baud_rate,parity,stop_bits,rx_idle_chars,tx_idle_chars):
+  global value
+  value = "set_modbus_tool_communication("+str(enabled)+str(baud_rate)+str(parity)+str(stop_bits)+str(rx_idle_chars)+str(tx_idle_chars)+")"
+  return value
 
-def cancelpopup():
-  global isShowing
-  isShowing = False
-  return isShowing
+def get_modbus_tool_communication():
+  global value
+  return value
 
+def init_modbus_communication(port,slaveaddress):
+  global instrument
+  instrument = modbus.Instrument(port,slaveaddress)
+  return True
+
+def tool_modbus_write(register_address, data, number_of_decimals):
+  try:
+    instrument.write_register(register_address,data,number_of_decimals)
+  except Exception:
+    Logger.error("Error in modbus write method", exc_info=True)
+    return "Modbus failed writing"
+  return "Succesfully executed!"
+
+def tool_modbus_read(register_address,number_of_decimals):
+  try:
+    value = str(instrument.read_register(register_address,number_of_decimals))
+  except Exception:
+    Logger.error("Error in modbus read method", exc_info=True)
+    value = "Modbus falied reading"
+  return value
 
 
 class MultithreadedSimpleXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
@@ -36,10 +57,11 @@ server = MultithreadedSimpleXMLRPCServer((LOCALHOST, 40408))
 server.RequestHandlerClass.protocol_version = "HTTP/1.1"
 print "Listening on port 40408..."
 
-server.register_function(showpopup,"showpopup")
-server.register_function(cancelpopup,"cancelpopup")
-server.register_function(isEnabled,"isEnabled")
-
+server.register_function(set_modbus_tool_communication,"set_modbus_tool_communication")
+server.register_function(get_modbus_tool_communication,"get_modbus_tool_communication")
+server.register_function(init_modbus_communication,"init_modbus_communication")
+server.register_function(tool_modbus_read,"tool_modbus_read")
+server.register_function(tool_modbus_write,"tool_modbus_write")
 
 server.serve_forever()
 
